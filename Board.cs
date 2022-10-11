3e444cd50eb11d3e83b76f5ded7d8cb0d7267357
KogolPL2001 <kornelg2001@wp.pl>
2022-10-11 13:49:09 +0200

    Second player added

---
+++ Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arcade
+{
+    public class Board
+    {
+        private int width;
+        private int height;
+        public Board() { }
+        public Board(int w, int h) { this.width = w; this.height = h; }
+
+        public void WriteBoard()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write('█');
+            }
+            for (int i = 0; i < height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("█");
+                Console.SetCursorPosition(width, i);
+                Console.Write("█");
+            }
+            for (int i = 0; i <= width; i++)
+            {
+                Console.SetCursorPosition(i, height);
+                Console.Write('█');
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace Arcade
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Arcade arcade = new Arcade(30, 30);
+            Arcade arcade = new Arcade(80, 30);
             arcade.Run();
         }
     }
+   
     public class Arcade
     {
         private int width;
         private int height;
-        SpaceShip spaceship;
+        SpaceShip spaceship1,spaceship2;
         Board board;
         ConsoleKeyInfo keyInfo;
         ConsoleKey consoleKey;
@@ -28,7 +29,8 @@ namespace Arcade
         }
         public void Setup()
         {
-            spaceship = new SpaceShip(width,height);
+            spaceship1 = new SpaceShip(width,height);
+            spaceship2 = new SpaceShip(width, 5);
             keyInfo = new ConsoleKeyInfo();
             consoleKey = new ConsoleKey();
         }
@@ -42,6 +44,7 @@ namespace Arcade
         }
         public void Run()
         {
+            int i=20;
             while (true)
             {
                 Console.Clear();
@@ -53,117 +56,27 @@ namespace Arcade
                     switch (consoleKey)
                     {
                         case ConsoleKey.A:
-                            spaceship.moveLeft();
+                            spaceship1.moveLeft();
                             break;
                         case ConsoleKey.D:
-                            spaceship.moveRight();
+                            spaceship1.moveRight();
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            spaceship2.moveLeft();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            spaceship2.moveRight();
                             break;
                     }
                     consoleKey = ConsoleKey.X;
-                    spaceship.WriteSpaceShip();
-                    spaceship.WriteFlame();
+
+                    spaceship1.WriteSpaceShip();
+                    spaceship1.WriteFlame();
+                    spaceship2.WriteSpaceShip();
+                    spaceship2.WriteFlame();
                     Thread.Sleep(1000/60);
                 }
             }
         }
-    }
-    public class Board
-    {
-        private int width;
-        private int height;
-        public Board() { }
-        public Board(int w, int h) { this.width = w; this.height = h; }
-
-        public void WriteBoard()
-        {
-            for (int i = 0; i < width; i++)
-            {
-                Console.SetCursorPosition(i, 0);
-                Console.Write('█');
-            }
-            for (int i = 0; i < height; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                Console.Write("█");
-                Console.SetCursorPosition(width, i);
-                Console.Write("█");
-            }
-            for (int i = 0; i <= width; i++)
-            {
-                Console.SetCursorPosition(i, height);
-                Console.Write('█');
-            }
-
-        }
-    }
-    public class SpaceShip
-    {
-        private char[,] body = new char[3, 3];
-        private int cursorX,cursorY;
-        private int width,height;
-        public SpaceShip(int width,int height)
-        {
-            body[1, 0] = '▲';
-            body[0, 1] = '<';
-            body[1, 1] = '█';
-            body[2, 1] = '>';
-            body[1, 2] = '#';
-            cursorX = (width - 2) / 2;
-            cursorY = height - 4;
-            this.width = width;
-            this.height = height;
-        }
-        public void moveLeft()
-        {
-            if (cursorX > 1)
-            {
-                Console.SetCursorPosition(cursorX, cursorY);
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        Console.SetCursorPosition(cursorX + i, cursorY + j);
-                        Console.Write(' ');
-                    }
-                }
-                cursorX--;
-            }
-        }
-        public void moveRight()
-        {
-            if (cursorX < width - 3)
-            {
-                Console.SetCursorPosition(cursorX, cursorY);
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        Console.SetCursorPosition(cursorX + i, cursorY + j);
-                        Console.Write(' ');
-                    }
-                }
-                cursorX++;
-            }
-        }
-        public void WriteSpaceShip()
-        {
-            Console.SetCursorPosition(cursorX,cursorY);
-            for (int i = 0; i<3; i++)
-            {
-                for(int j=0;j<3;j++)
-                {
-                    Console.SetCursorPosition(cursorX+i, cursorY+j);
-                    Console.Write(body[i, j]);
-                }
-            }
-        }
-        public void WriteFlame()
-        {
-            string chars = "$#&";
-            Random rand = new Random();
-            int num = rand.Next(0, chars.Length);
-            body[1, 2] = chars[num];
-        }
-
     }
 }
